502f64dda3e669dd15361c951329a6217122c56f
jackhildebrand <126699344+jackhildebrand@users.noreply.github.com>
2023-03-31 15:58:31 +0000

    added image

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <img src='Octocat.png' height={300} width={350} alt='img'></img>
       <h1 className='playlistName'>
-        yes
+        Playlist
       </h1>
       <h1 className='userName'>
         Jack Hildebrand
@@ -13,10 +13,31 @@ function App() {
       <h1 className='updated'>
         UPDATED MONDAY
       </h1>
-      <button className='playButton'>Play</button>
-      <button className='shuffleButton'>Shuffle</button>
-      <p className='song'>Jungle</p>
-      <p className='song'>way back</p>
+      <button className='playButton'><span className='icon'></span><a href='#'></a>Play</button>
+      <button className='shuffleButton'><i className='shuffleIcon'></i>Shuffle</button>
+      <hr></hr>
+      <div className='wrapper'>
+        <div className='centered'>
+          <p className='song'>song 1</p>
+          <div className='left'>
+            <img src='Octocat.png' alt='song' height={75} width={90}></img>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <p className='song'>song 2</p>
+      <hr></hr>
+      <p className='song'>song 3</p>
+      <hr></hr>
+      <p className='song'>song 4</p>
+      <hr></hr>
+      <p className='song'>song 5</p>
+      <hr></hr>
+      <p className='song'>song 6</p>
+      <hr></hr>
+      <p className='song'>song 7</p>
+      <hr></hr>
+      <p className='song'>song 8</p>
     </div>
   );
 }
